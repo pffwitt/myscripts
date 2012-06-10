68ef34fe073e5e82adc7b0c9c63e2d452d0b18b7
pffwitt <pffwitt@gmail.com>
2012-06-10 23:24:53 +0200

    Cleaning

--- wawawa.py
+++ wawawa.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-import StringIO
-import argparse
-import re
-import urllib2
+################################################################
+# Web Spidering / Crawling
+#
+################################################################
+import StringIO, argparse, re, urllib2
 
-# args
+# Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--url', action='store', dest='url', help='Url to check')
 arg = parser.parse_args()
@@ -13,21 +14,21 @@ if not arg.url:
     parser.print_help()
     exit(1)
 
-# return url content
+# Functions
 def geturl(url):
+    '''Return a web page content'''
     usock = urllib2.urlopen(url)
     page = usock.read()
     usock.close()
-    return page
+    return StringIO.StringIO(page)
 
 # find links in page content
 regexlinks = re.compile(r"(https?://[^\"\'<>\s]+)")
 
 links_found = []
 
-def findlinks(content):
+def findlinks(page):
     links = []
-    page = StringIO.StringIO(content)
     
     for line in page.readlines():
         match = regexlinks.search(line)
@@ -52,8 +53,9 @@ while len(links_collection) > 0:
     crawl_links = findlinks(link_content)
     for crawl_link in crawl_links:
         if crawl_link not in links_visited and crawl_link not in links_collection:
-            print "new link found : %s" % crawl_link
             links_collection.append(crawl_link)
-        else:
-            print "%s already in links_visited" % crawl_link
-            
+
+# report
+print "The following links have been crawled :\n"
+for link_visited in links_visited:
+    print "\t- %s" % link_visited
